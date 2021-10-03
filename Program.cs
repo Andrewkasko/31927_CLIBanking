efb0fba071f7a36c8283bf8e8814ca54bc13afea
Andrew Kaskaniotis <akaskaniotis@gmail.com>
2021-10-04 04:11:51 +1100

    Masked Password

--- Program.cs
+++ Program.cs
@@ -122,7 +122,40 @@ namespace DotNetAssignment1_31927
                 for (int field = 0; field < item; field++)
                 {
                     Console.SetCursorPosition(loginFieldPos[field, 0], loginFieldPos[field, 1]);
-                    loginUserInputs[field] = Console.ReadLine();
+                    if (field == 0)
+                    {
+                        loginUserInputs[field] = Console.ReadLine();
+                    }
+                    else {
+                        loginUserInputs[1] = "";
+                        ConsoleKeyInfo info = Console.ReadKey(true);
+                        while (info.Key != ConsoleKey.Enter)
+                        {
+                            if (info.Key != ConsoleKey.Backspace)
+                            {
+                                Console.Write("*");
+                                loginUserInputs[1] += info.KeyChar;
+                            }
+                            else if (info.Key == ConsoleKey.Backspace)
+                            {
+                                if (!string.IsNullOrEmpty(loginUserInputs[1]))
+                                {
+                                    // remove one character from the list of password characters
+                                    loginUserInputs[1] = loginUserInputs[1].Substring(0, loginUserInputs[1].Length - 1);
+                                    // get the location of the cursor
+                                    int pos = Console.CursorLeft;
+                                    // move the cursor to the left by one character
+                                    Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                                    // replace it with space
+                                    Console.Write(" ");
+                                    // move the cursor to the left by one character again
+                                    Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                                }
+                            }
+                            info = Console.ReadKey(true);
+                        }
+                    }
+                    
                 }
 
                 if (Auth.CheckPassword(loginUserInputs[0], loginUserInputs[1]))
